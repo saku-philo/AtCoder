14aef91e29c9922cb3c39102341e7515cde9776e
hiroyuki_sakurada <sanno.phill@gmail.com>
2021-05-01 11:54:33 +0900

    隣接行列解法作成

--- PAST3/e.py
+++ PAST3/e.py
@@ -26,7 +26,7 @@ for i in range(0, M):
     graph[v][u] = True
 
 # 頂点の色のリストを受け取る
-C = list(map(int, input().splti()))
+C = list(map(int, input().split()))
 
 # Q個のクエリを受け取る
 for i in range(0, Q):
@@ -36,5 +36,30 @@ for i in range(0, Q):
     if query[0] == 1:
         x = query[1]
 
+        # 頂点番号は全てデクリメントする
+        x -= 1
 
-print(graph)
+        # 頂点xの色を出力する
+        print(C[x])
+
+        #全ての頂点を順番に見る
+        for i in range(0, N):
+            # 頂点iが頂点xに隣接しているとき、
+            # すなわち頂点xと頂点iの間に辺がある時
+            if graph[x][i]:
+                # 頂点iの色をC[x]に書き換える
+                C[i] = C[x]
+
+    # スプリンクラーを起動しないクエリの場合
+    if query[0] == 2:
+        x = query[1]
+        y = query[2]
+
+        # 頂点番号は全てデクリメントする
+        x -= 1
+
+        # 頂点xの色を出力する
+        print(C[x])
+
+        # 頂点xの色をyに書き換える
+        C[x] = y
